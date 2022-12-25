eb1a07c2557e10c9651a287d15c67c2c45ef50e5
abidhazratzai <114306494+abidhazratzai@users.noreply.github.com>
2022-12-24 23:33:47 -0500

    Delete src directory

--- src/dau01.java
+++
@@ -1,9 +0,0 @@
-public class dau01 {
-    public static void main(String[] args) {
-
-
-        System.out.println("0787002020");
-        System.out.println("7036069202");
-        System.out.println("Abid hazratzai");
-    }
-}
